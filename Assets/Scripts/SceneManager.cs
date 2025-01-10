c825511ccbb044b9a5e74861f571c06888c5ce73
박성환 <johnpark@zempot.com>
2025-01-10 11:10:16 +0900

    base project

---
+++ Assets/Scripts/SceneManager.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Jenny
+{
+    public class SceneManager : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+        
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+        
+        }
+    }
+}
