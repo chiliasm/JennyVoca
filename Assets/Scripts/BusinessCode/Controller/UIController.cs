cee860ed86668fe5d6b9948ffff37166cafe969d
[개발]박성환 <johnpark@zempot.com>
2025-01-13 19:09:57 +0900

    코드정리

--- Assets/Scripts/BusinessCode/Controller/UIController.cs
+++ Assets/Scripts/BusinessCode/Controller/UIController.cs
@@ -14,7 +14,7 @@ namespace Jenny
         #endregion
 
 
-        #region // [Func] MainUI //
+        #region // [Func] OpenUI //
         public void OpenUI(E_MainUI type)
         {
             //  MainUI 타입이 다르면 SubUI는 모두 닫는다.
@@ -25,30 +25,28 @@ namespace Jenny
             _mainUIController.OpenUI(type);
         }
 
-        public void CloseUI(E_MainUI type)
-        {
-            _mainUIController.CloseUI(type);
-        }
-        #endregion
-
-        #region // [Func] SubUI //
         public void OpenUI(E_SubUI type)
         {
             _subUIController.OpenUI(type);
         }
 
-        public void CloseUI(E_SubUI type)
-        {
-            _subUIController.CloseUI(type);
-        }
-        #endregion
-
-        #region // [Func] MsgUI //
         public void OpenUI(E_MsgUI type)
         {
             _msgUIController.OpenUI(type);
         }
+        #endregion
 
+        #region // [Func] CloseUI //
+        public void CloseUI(E_MainUI type)
+        {
+            _mainUIController.CloseUI(type);
+        }
+        
+        public void CloseUI(E_SubUI type)
+        {
+            _subUIController.CloseUI(type);
+        }
+        
         public void CloseUI(E_MsgUI type)
         {
             _msgUIController.CloseUI(type);
