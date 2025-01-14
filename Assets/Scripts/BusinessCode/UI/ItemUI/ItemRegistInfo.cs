a20eced2f9bc73a9d4c69c86d069af974d7696fb
[개발]박성환 <johnpark@zempot.com>
2025-01-14 18:08:12 +0900

    MainUI RegistUI 작업중

---
+++ Assets/Scripts/BusinessCode/UI/ItemUI/ItemRegistInfo.cs
@@ -0,0 +1,118 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Jenny
+{
+    public class ItemRegistInfo : BaseMonoBehaviour
+    {
+        #region // [Var] Unity //
+        [Header("== ItemRegistInfo ==")]
+        [SerializeField]
+        Button _btnBg;
+        [SerializeField]
+        Image _imgBg;
+        [SerializeField]
+        TMP_Text _textEn;
+        [SerializeField]
+        TMP_Text _textKr;
+        [SerializeField]
+        GameObject _goBtnGroup;
+        [SerializeField]
+        Button _btnModify;
+        [SerializeField]
+        Button _btnDelete;
+        #endregion
+
+        #region // [Var] Data //
+        int mID;
+        public int ID { get { return mID; } }
+        MainUI_Lobby_Regist.RegistInfo mInfo;
+        bool mIsSelected = false;
+
+        System.Action<int> mSelectCallback;
+        System.Action<int> mModifyCallback;
+        System.Action<int> mDeleteCallback;
+
+        readonly Color COLOR_BG_DEFAULT = new(0.96f, 0.85f, 0.59f, 1f);
+        readonly Color COLOR_BG_SELECT = new(0.85f, 0.85f, 0.85f, 1f);
+        #endregion
+
+
+        #region // [Func] Unity //
+        protected override void OnEnable()
+        {
+            base.OnEnable();
+
+            _btnBg.onClick.AddListener(OnClickBgButton);
+            _btnModify.onClick.AddListener(OnClickModifyButton);
+            _btnDelete.onClick.AddListener(OnClickDeleteButton);
+        }
+
+        protected override void OnDisable()
+        {
+            base.OnDisable();
+
+            _btnBg.onClick.RemoveListener(OnClickBgButton);
+            _btnModify.onClick.RemoveListener(OnClickModifyButton);
+            _btnDelete.onClick.RemoveListener(OnClickDeleteButton);
+        }
+        #endregion
+
+        #region // [Func] Set //
+        public void SetData(int id, MainUI_Lobby_Regist.RegistInfo info, System.Action<int> lpSelectCallback = null, System.Action<int> lpModifyCallback = null, System.Action<int> lpDeleteCallback = null)
+        {
+            mID = id;
+            mInfo = info;
+            mSelectCallback = lpSelectCallback;
+            mModifyCallback = lpModifyCallback; 
+            mDeleteCallback = lpDeleteCallback;
+            mIsSelected = false;
+
+            UpdateUI();
+        }
+
+        public void SetSelect(bool isSelect)
+        {
+            mIsSelected = isSelect;
+
+            UpdateUISelect();
+        }
+        #endregion
+
+        #region // [Func] UpdateUI //
+        void UpdateUI()
+        {
+            _textEn.text = mInfo.En;
+            _textKr.text = string.Format("({0})", mInfo.Kr);
+
+            UpdateUISelect();
+        }
+
+        void UpdateUISelect()
+        {
+            _goBtnGroup.SetActive(mIsSelected);
+            _imgBg.color = mIsSelected ? COLOR_BG_SELECT : COLOR_BG_DEFAULT;
+        }
+        #endregion
+
+        #region // [Func] Callback //
+        void OnClickBgButton()
+        {
+            mSelectCallback?.Invoke(mID);
+        }
+
+        void OnClickModifyButton()
+        {
+            mModifyCallback?.Invoke(mID);
+        }
+
+        void OnClickDeleteButton()
+        {
+            mDeleteCallback?.Invoke(mID);
+        }
+        #endregion
+    }
+}
---
+++ Assets/Scripts/BusinessCode/UI/MainUI/MainUI_Lobby_Regist.cs
@@ -0,0 +1,254 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Jenny
+{
+    public class MainUI_Lobby_Regist : MainUI
+    {
+        public class RegistInfo
+        {
+            public string En;
+            public string Kr;
+
+            public RegistInfo(string en, string kr)
+            {
+                En = en;
+                Kr = kr;
+            }
+        }
+
+        #region // [Var] Unity //
+        [Header("== MainUI Regist ==")]
+        [SerializeField]
+        TMP_Text _textTitle;
+        [SerializeField]
+        Button _btnClose;
+
+        [SerializeField]
+        TMP_InputField _inputEn;
+        [SerializeField]
+        TMP_InputField _inputKr;
+        [SerializeField]
+        Button _btnAdd;
+        [SerializeField]
+        Button _btnModify;
+
+        [SerializeField]
+        ScrollRect _scrollList;
+        [SerializeField]
+        GameObject _itemObject;
+        [SerializeField]
+        Transform _trItemPool;
+
+        [SerializeField]
+        Button _btnRegist;
+        #endregion
+
+        #region // [Var] Data //
+        readonly List<RegistInfo> mDataList = new();
+        int mModifyID = -1;
+
+        readonly List<ItemRegistInfo> mItemList = new();
+        readonly Queue<ItemRegistInfo> mItemPool = new();
+        #endregion
+
+
+        #region // [Func] Unity //
+        protected override void OnEnable()
+        {
+            base.OnEnable();
+
+            _btnAdd.onClick.AddListener(OnClickAddButton);
+            _btnModify.onClick.AddListener(OnClickModifyButton);
+            _btnClose.onClick.AddListener(OnClickCloseButton);
+            _btnRegist.onClick.AddListener(OnClickRegistButton);
+        }
+
+        protected override void OnDisable()
+        {
+            base.OnDisable();
+
+            _btnAdd.onClick.RemoveListener(OnClickAddButton);
+            _btnModify.onClick.RemoveListener(OnClickModifyButton);
+            _btnClose.onClick.RemoveListener(OnClickCloseButton);
+            _btnRegist.onClick.RemoveListener(OnClickRegistButton);
+
+            mDataList.Clear();
+        }
+
+        protected override void Update()
+        {
+            base.Update();
+        }
+        #endregion
+
+        #region // [Func] Init //
+        void InitValue()
+        {
+            mModifyID = -1;
+            _inputEn.text = string.Empty;
+            _inputKr.text = string.Empty;
+
+            foreach (var it in mItemList)
+                it.SetSelect(false);
+        }
+        #endregion
+
+        #region // [Func] UpdateUI //
+        void UpdateUI()
+        {
+            UpdateUIButton();
+            UpdateUIItemList();
+        }
+
+        void UpdateUIButton()
+        {
+            var isAdd = (mModifyID < 0);
+
+            _btnAdd.gameObject.SetActive(isAdd);
+            _btnModify.gameObject.SetActive(!isAdd);
+        }
+
+        void UpdateUIItemList()
+        {
+            RemoveAllItem();
+
+            int id = 0;
+            foreach (var it in mDataList)
+            {
+                var item = GetOrNewItem();
+                if (item != null)
+                {
+                    item.SetData(id, it, OnSelectedCallback, OnModifyCallback, OnDeleteCallback);
+                    item.gameObject.SetActive(true);
+                    id++;
+                }
+            }
+        }
+        #endregion
+
+        #region // [Func] Scroll //
+        ItemRegistInfo GetOrNewItem()
+        {
+            ItemRegistInfo info = null;
+            if (mItemPool.Count > 0)
+                info = mItemPool.Dequeue();
+            else
+            {
+                var o = GameObject.Instantiate(_itemObject);
+                if (o.TryGetComponent<ItemRegistInfo>(out var comp))
+                    info = comp;
+            }
+            if (info != null)
+            {
+                info.transform.SetParent(_scrollList.content);
+                info.transform.SetAsLastSibling();
+            }
+            return info;
+        }
+
+        void RemoveItem(ItemRegistInfo info)
+        {
+            if (info != null)
+            {
+                info.gameObject.SetActive(false);
+                info.transform.SetParent(_trItemPool);
+                mItemPool.Enqueue(info);
+            }   
+        }
+
+        void RemoveAllItem()
+        {
+            foreach (var it in mItemList)
+                RemoveItem(it);
+            mItemList.Clear();
+        }
+        #endregion
+
+        #region // [Func] Callback //
+        void OnClickCloseButton()
+        {
+            UIManager.Instance.CloseUI(E_MainUI.Last);
+        }
+
+        void OnClickRegistButton()
+        {
+
+        }
+
+        void OnClickAddButton()
+        {
+            var en = _inputEn.text;
+            var kr = _inputKr.text;
+            if (string.IsNullOrWhiteSpace(en) || string.IsNullOrWhiteSpace(kr))
+                return;
+            foreach (var it in mDataList)
+            {
+                if (en.Equals(it.En))
+                    return;
+            }
+
+            mDataList.Add(new(en, kr));
+
+            InitValue();
+            UpdateUI();
+        }
+
+        void OnClickModifyButton()
+        {
+            if (mDataList.Count <= mModifyID || mModifyID < 0 )
+                return;
+
+            var info = mDataList[mModifyID];
+            if (info != null)
+            {
+                info.En = _inputEn.text;
+                info.Kr = _inputKr.text;
+
+                InitValue();
+                UpdateUI();
+            }
+        }
+
+        void OnSelectedCallback(int id = -1)
+        {
+            if (mItemList.Count <= id)
+                return;
+            
+            foreach (var it in mItemList)
+                it.SetSelect(it.ID.Equals(id));
+        }
+
+        void OnModifyCallback(int id = -1)
+        {
+            if (mDataList.Count <= id || id < 0)
+                return;
+
+            var info = mDataList[id];
+            if (info != null)
+            {
+                _inputEn.text = info.En;
+                _inputKr.text = info.Kr;
+
+                mModifyID = id;
+                UpdateUI();
+            }
+        }
+
+        void OnDeleteCallback(int id = -1)
+        {
+            if (mDataList.Count <= id || id < 0)
+                return;
+
+            mDataList.RemoveAt(id);
+
+            InitValue();
+            UpdateUI();
+        }
+        #endregion
+    }
+}
